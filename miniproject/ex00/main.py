b2fea08323a0e9f3508bd455f391ce155658a038
kimHobbyv1 <GitHub>
2026-09-24 20:44:04 +0700

    up

--- miniproject/ex00/main.py
+++ miniproject/ex00/main.py
@@ -1,15 +1,66 @@
 from checkmate import checkmate
 
 def main():
-    board = """\
-....
-..K.
-....
-Q...\
-"""
-    print(board)
-    checkmate(board)
+
     
+
+    board1 = """\
+R...
+.K..
+..P.
+....\
+"""
+    print("Board1")
+    checkmate(board1)
+
+    board2 = """\
+...
+.K.
+...\
+"""
+    print("Board2")
+    checkmate(board2)
+
+    board3 = """\
+ABCD
+12K4
+กข..
+..คง\
+"""
+    print("Board3")
+    checkmate(board3)
+
+    board4 = ""
+    print("Board4")
+    checkmate(board4)
+
+    board5 = """\
+.....
+.K...
+.....\
+"""
+    print("Board5")
+    checkmate(board5)
+
+    board6 = """\
+🤡R🤡🤡
+🤡🤡K🤡
+🤡🤡🤡P
+🤡🤡🤡🤡\
+"""
+    print("Board6")
+    checkmate(board6)
+
+    board7 = """\
+K..K
+.KK.
+.KK.
+KKKK\
+"""
+    print("Board7")
+    checkmate(board7)
+
+
 
 if __name__ == "__main__":
     main()
